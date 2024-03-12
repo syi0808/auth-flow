828e5bbf0cad43c64350dea2aa91b8e0dedd5707
Sung YeIn <syi9397@naver.com>
2024-03-12 15:07:13 +0900

    초기 환경 셋업 (with create-next-app)

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@/shared/styles/reset.css';
+import QueryProvider from '@/queries';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '인증 페이지',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
