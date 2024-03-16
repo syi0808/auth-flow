601ec555bdc07fa0fc23b0c2d279a3e8dbb95b4b
Sung YeIn <syi9397@naver.com>
2024-03-16 22:04:16 +0900

    feat: 이미지 업로드 API 구현

--- src/app/api/auth/profile/image/route.ts
+++ src/app/api/auth/profile/image/route.ts
@@ -1,6 +1,25 @@
 import { prisma } from '@/utils/server/db';
 import { authenticate } from '@/utils/server/middleware';
+import { writeFile } from 'fs/promises';
+import { NextResponse } from 'next/server';
+import path from 'path';
 
-export const PUT = authenticate(async function (request: Request) {
-  const {} = await request.json();
+const imageSourceDirectory = path.join(process.cwd(), 'images/');
+
+export const POST = authenticate(async function (request, { tokenPayload }) {
+  const formData = await request.formData();
+
+  const image = formData.get('image') as File;
+  const buffer = Buffer.from(await image.arrayBuffer());
+  const filename = Date.now() + image.name.replaceAll(' ', '_');
+
+  try {
+    await writeFile(path.join(imageSourceDirectory, filename), buffer);
+
+    await prisma.user.update({ data: { image: filename }, where: { id: tokenPayload.id } });
+
+    return new NextResponse(null, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: `${error}` }, { status: 500 });
+  }
 });
