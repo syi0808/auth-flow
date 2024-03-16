9dd94bb2879342e99adf3cf7ea4111cef9101d3a
Sung YeIn <syi9397@naver.com>
2024-03-16 22:05:03 +0900

    refactor: Middleware 수정사항 반영

--- src/app/api/auth/profile/route.ts
+++ src/app/api/auth/profile/route.ts
@@ -3,8 +3,8 @@ import { authenticate } from '@/utils/server/middleware';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 
-export const GET = authenticate(async function (request) {
-  const user = await prisma.user.findFirst({ where: { id: request.payload.id } });
+export const GET = authenticate(async function (_request, { tokenPayload }) {
+  const user = await prisma.user.findFirst({ where: { id: tokenPayload.id } });
 
   return NextResponse.json(user);
 });
@@ -13,13 +13,13 @@ const partialUser = z.object({
   name: z.string().optional(),
 });
 
-export const PUT = authenticate(async function (request) {
+export const PUT = authenticate(async function (request, { tokenPayload }) {
   const body = request.json();
 
   try {
     const response = partialUser.parse(body);
 
-    const user = await prisma.user.update({ data: response, where: { id: request.payload.id } });
+    const user = await prisma.user.update({ data: response, where: { id: tokenPayload.id } });
 
     return NextResponse.json(user);
   } catch (error) {
