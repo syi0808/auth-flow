263a42b5c70add31515d5f4903a5f4e29904280d
Sung YeIn <syi9397@naver.com>
2024-03-16 21:01:53 +0900

    feat: 프로필 API 구현

--- src/app/api/auth/profile/route.ts
+++ src/app/api/auth/profile/route.ts
@@ -1,10 +1,28 @@
 import { prisma } from '@/utils/server/db';
 import { authenticate } from '@/utils/server/middleware';
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
 
 export const GET = authenticate(async function (request) {
-  const {} = await request.json();
+  const user = await prisma.user.findFirst({ where: { id: request.payload.id } });
+
+  return NextResponse.json(user);
+});
+
+const partialUser = z.object({
+  name: z.string().optional(),
 });
 
 export const PUT = authenticate(async function (request) {
-  const {} = await request.json();
+  const body = request.json();
+
+  try {
+    const response = partialUser.parse(body);
+
+    const user = await prisma.user.update({ data: response, where: { id: request.payload.id } });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return new NextResponse(null, { status: 400 });
+  }
 });
