80efdb7f7a7686e46a21c811dce8d8c7c8941e92
Sung YeIn <syi9397@naver.com>
2024-03-16 22:04:27 +0900

    refactor: Middleware 리팩토링

--- src/utils/server/middleware.ts
+++ src/utils/server/middleware.ts
@@ -1,17 +1,18 @@
-'use client';
-
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { JwtPayload, verify } from 'jsonwebtoken';
 
-export const authenticate = async (next: (request: Request & { payload: JwtPayload }) => any) => (request: Request) => {
-  const token = cookies().get('Authorization');
+type Handler<T> = (request: Request, payload: T) => Promise<any>;
+type Middleware<T> = (next: Handler<T>) => (request: Request) => Promise<NextResponse> | ReturnType<Handler<T>>;
+
+export const authenticate: Middleware<{ tokenPayload: JwtPayload }> = (next) => async (request) => {
+  const token = cookies().get('token')?.value;
 
   if (token) {
     try {
       const payload = verify(token as unknown as string, process.env.JWT_SECRET as string) as JwtPayload;
 
-      return next({ ...request, payload });
+      return await next(request, { tokenPayload: payload });
     } catch (err) {
       return NextResponse.json({ error: `${err}` }, { status: 500 });
     }
