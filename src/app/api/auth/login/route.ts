497f491e5f854a6dbfd398526e9cf55222f4420b
Sung YeIn <syi9397@naver.com>
2024-03-16 20:00:01 +0900

    feat: 로그인 API 구현

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,5 +1,35 @@
 import { prisma } from '@/utils/server/db';
+import { compareSync } from 'bcrypt';
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
+import { sign } from 'jsonwebtoken';
+
+const loginBody = z.object({
+  id: z.string(),
+  password: z.string(),
+});
 
 export async function POST(request: Request) {
-  const {} = await request.json();
+  const data = await request.json();
+  const response = loginBody.safeParse(data);
+
+  if (!response.success) {
+    return NextResponse.json({ error: response.error }, { status: 400 });
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: response.data.id,
+    },
+  });
+
+  if (user && compareSync(response.data.password, user.password)) {
+    const { password, ...userWithoutPassword } = user;
+
+    const token = sign({ ...userWithoutPassword }, process.env.JWT_SECRET as string, { expiresIn: '1d' });
+
+    return NextResponse.json({ token });
+  }
+
+  return new NextResponse(null, { status: 400 });
 }
