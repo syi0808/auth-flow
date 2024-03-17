ffd421bca791895f0609379c4f050ddd68b2e726
Sung YeIn <syi9397@naver.com>
2024-03-17 10:38:24 +0900

    fix: image 서빙 API 예외처리

--- src/app/api/image/[imageName]/route.ts
+++ src/app/api/image/[imageName]/route.ts
@@ -7,7 +7,11 @@ const imageSourceDirectory = path.join(process.cwd(), 'images/');
 export async function GET(_request: Request, { params }: { params: { imageName: string } }) {
   const { imageName } = params;
 
-  const image = await readFile(path.join(imageSourceDirectory, imageName));
+  try {
+    const image = await readFile(path.join(imageSourceDirectory, imageName));
 
-  return new NextResponse(image);
+    return new NextResponse(image);
+  } catch (error) {
+    return NextResponse.json({ error: `${error}` }, { status: 400 });
+  }
 }
